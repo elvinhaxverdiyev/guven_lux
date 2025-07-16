252b410071f8924b1d49ee43a72bbdc434819024
Elvin <elvinhaxverdiyev777@gamil.com>
2025-07-16 21:42:14 +0400

    fixed some bugs

--- guvenlux/guvenlux/settings.py
+++ guvenlux/guvenlux/settings.py
@@ -118,9 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS üçün BASE_DIR - Path tipində olduğundan onunla uyğun yazmaq üçün:
 STATICFILES_DIRS = [
-    BASE_DIR / 'static/assets',
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
