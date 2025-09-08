0c76c5b4b05462c762651a11786d0dab1e5fbc04
musfiqamirov888 <musfiq.emirov5@gmail.com>
2025-09-08 16:25:35 +0400

    setting file updated

--- guvenlux/guvenlux/settings.py
+++ guvenlux/guvenlux/settings.py
@@ -28,12 +28,17 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 # Application definition
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://guven-lux.az",
+    "https://www.guven-lux.az"
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -61,7 +66,9 @@ if not DEBUG:
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    
+    SECURE_HSTS_SECONDS = 63072000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
 
 ROOT_URLCONF = 'guvenlux.urls'
 
