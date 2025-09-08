c892c7033f70113375c475bdafc8ef571cf46687
musfiqamirov888 <musfiq.emirov5@gmail.com>
2025-09-08 16:53:06 +0400

    setting file updated

--- guvenlux/guvenluxapp/admin/employee.py
+++ guvenlux/guvenluxapp/admin/employee.py
@@ -7,12 +7,22 @@ from ..models.employee import Employee
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('name', 'photo_preview')
     readonly_fields = ('photo_preview',)
-    exclude = ('position', 'phone', 'company',)
+    exclude = ('position', 'phone', 'company',)  # hələ də exclude saxla
     search_fields = ('name', 'company__name')
     list_filter = ('company',)
 
+    def save_model(self, request, obj, form, change):
+        if not obj.company:
+            # Məsələn, ilk şirkəti avtomatik təyin et
+            from .company import Company
+            obj.company = Company.objects.first()
+        super().save_model(request, obj, form, change)
+
     def photo_preview(self, obj):
         if obj.photo:
-            return format_html('<img src="{}" width="60" height="60" style="object-fit: cover; border-radius: 5px;" />', obj.photo.url)
+            return format_html(
+                '<img src="{}" width="60" height="60" style="object-fit: cover; border-radius: 5px;" />',
+                obj.photo.url
+            )
         return "-"
     photo_preview.short_description = 'Şəkil Önizləmə'
