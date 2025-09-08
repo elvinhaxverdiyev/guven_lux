22e1f1d95cbaa80ee741b19edad5dd954e1bc39b
musfiqamirov888 <musfiq.emirov5@gmail.com>
2025-09-08 18:30:10 +0400

    updated

--- guvenlux/apis/company.py
+++ guvenlux/apis/company.py
@@ -16,18 +16,18 @@ class CompanyOverviewView(View):
     View for displaying the company overview page.
 
     Retrieves:
-    - All companies with their related employees (using prefetch for optimization).
+    - All companies
     - The latest background image for the 'about' page.
 
     Context sent to the template:
-    - companies: QuerySet of Company objects with preloaded employees.
+    - companies: QuerySet of Company objects
     - bg_image: The most recently created BackgroundImage for the 'about' page.
 
     Template:
     - about.html
     """
     def get(self, request):
-        companies = Company.objects.prefetch_related('employees').all()
+        companies = Company.objects.all()
         bg_image = BackgroundImage.objects.filter(page='about').order_by('-created_at').first()
         return render(request, 'about.html', {
             'companies': companies,
--- guvenlux/guvenluxapp/models/company.py
+++ guvenlux/guvenluxapp/models/company.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-
 class Company(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
+    logo = models.ImageField(upload_to="companies/", blank=True, null=True)  # əgər logo da lazım olsa
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Shirket'
-        verbose_name_plural = 'Shirketler'
+        verbose_name = "Şirkət"
+        verbose_name_plural = "Şirkətlər"
