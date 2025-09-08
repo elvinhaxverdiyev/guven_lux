f65a3792c430318d1795f69d2d06ba9bdefd70de
musfiqamirov888 <musfiq.emirov5@gmail.com>
2025-09-08 17:08:38 +0400

    setting file updated

--- guvenlux/guvenluxapp/admin/employee.py
+++ guvenlux/guvenluxapp/admin/employee.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from ..models.employee import Employee
+from ..models.company import Company 
 
 
 @admin.register(Employee)
@@ -16,3 +17,8 @@ class EmployeeAdmin(admin.ModelAdmin):
             return format_html('<img src="{}" width="60" height="60" style="object-fit: cover; border-radius: 5px;" />', obj.photo.url)
         return "-"
     photo_preview.short_description = 'Şəkil Önizləmə'
+
+    def save_model(self, request, obj, form, change):
+        if not obj.company:
+            obj.company = Company.objects.first()  # və ya default şirkət
+        super().save_model(request, obj, form, change)
